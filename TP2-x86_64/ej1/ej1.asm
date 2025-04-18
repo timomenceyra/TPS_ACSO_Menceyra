0cc1f879f7b7c391bf125335880d8352d88f4e65
timomenceyra <tmenceyra@udesa.edu.ar>
2025-04-18 13:21:34 -0300

    correccion funciones asm

--- TP2-x86_64/ej1/ej1.asm
+++ TP2-x86_64/ej1/ej1.asm
@@ -23,6 +23,7 @@ string_proc_list_create_asm:
     mov rdi, 16              ; tamaño del struct
     call malloc              ; devuelve puntero en rax
 
+
     test rax, rax            ; ¿es NULL?
     je .ret_null             ; si sí, devolvemos NULL
 
@@ -30,78 +31,125 @@ string_proc_list_create_asm:
     mov QWORD [rax], 0       ; first
     mov QWORD [rax + 8], 0   ; last
 
-.ret_null:
+    ; Devolvemos el puntero a la lista
+    ret
+
+.malloc_failed:
+    ; Manejo de error de malloc
+    xor rax, rax             ; si malloc falla, devolvemos NULL
     ret
 
 string_proc_node_create_asm:
-    ; Guardamos el argumento en rdi (value)
-    push rdi                 ; salvamos el valor original en stack
+    ; Argumentos: rdi = type (uint8_t), rsi = hash (char*)
+    ; Llamamos a malloc(sizeof(string_proc_node)) = malloc(32)
+    push rdi                 ; guardamos type
+    push rsi                 ; guardamos hash
+    mov rdi, 32              ; tamaño del struct
+    call malloc              ; devuelve puntero en rax
+    pop rsi                  ; recuperamos hash
+    pop rdi                  ; recuperamos type
 
-    ; Llamamos a malloc(16)
-    mov rdi, 16
-    call malloc              ; malloc devuelve puntero en rax
+    test rax, rax            ; ¿es NULL?
+    je .ret_null             ; si sí, devolvemos NULL
 
-    test rax, rax
-    je .ret_null             ; si malloc falla, devolvemos NULL
+    ; Inicializamos los campos: node->next = NULL; node->prev = NULL; node->type = type; node->hash = hash
+    mov QWORD [rax], 0       ; next
+    mov QWORD [rax + 8], 0   ; prev
+    mov BYTE [rax + 16], dil ; type
+    mov QWORD [rax + 24], rsi; hash
 
-    ; Restauramos value y seteamos node->value = value
-    pop rdx                  ; rdx = value
-    mov [rax], rdx           ; node->value
-
-    ; node->next = NULL
-    mov QWORD [rax + 8], 0
-
+    ; Devolvemos el puntero al nodo
     ret
 
-.ret_null:
-    add rsp, 8               ; limpiamos la pila en caso de early return
+.malloc_failed:
+    ; Manejo de error de malloc
+    xor rax, rax             ; si malloc falla, devolvemos NULL
     ret
 
 string_proc_list_add_node_asm:
-    ; if (list->head == NULL)
-    mov rax, [rdi]           ; rax = list->head
-    test rax, rax
-    jne .else_case           ; si no es NULL, vamos al else
+    ; Argumentos: rdi = list, rsi = type (uint8_t), rdx = hash (char*)
 
-    ; list->head = node
-    mov [rdi], rsi
-    jmp .set_tail
+    ; Llamamos a string_proc_node_create_asm(type, hash)
+    push rdi             ; guardamos list
+    mov rdi, rsi         ; pasamos type
+    mov rsi, rdx         ; pasamos hash
+    call string_proc_node_create_asm
+    pop rdi              ; recuperamos list
 
-.else_case:
-    ; list->tail->next = node
-    mov rcx, [rdi + 8]       ; rcx = list->tail
-    mov [rcx + 8], rsi       ; tail->next = node
+    test rax, rax        ; ¿es NULL?
+    je .return           ; si sí, devolvemos NULL
 
-.set_tail:
-    ; list->tail = node
-    mov [rdi + 8], rsi
+
+    mov r8, rax        ; guardamos el nuevo nodo en r8
+    mov r9, [rdi]      ; r9 = list->first
+    test r9, r9        ; ¿list->first == NULL?
+    jz .empty_list     ; si sí, vamos a la lista vacía
+
+    ; Caso lista no vacía
+    mov r10, [rdi + 8] ; r10 = list->last
+    mov [r8 + 8], r10  ; new_node->prev = list->last
+    mov [r10], r8      ; list->last->next = new_node
+    mov [rdi + 8], r8  ; list->last = new_node
+    jmp .return
+
+.empty_list:
+    ; Caso lista vacía
+    mov [rdi], r8      ; list->first = new_node
+    mov [rdi + 8], r8  ; list->last = new_node
+
+.return:
     ret
 
 string_proc_list_concat_asm:
-    ; if (list1->head == NULL)
-    mov rax, [rdi]            ; rax = list1->head
-    test rax, rax
-    jne .else_case            ; if list1->head != NULL -> else
+    ; Argumentos: rdi = list, rsi, type (uint8_t), rdx = hash (char*)
 
-    ; list1->head = list2->head
-    mov rcx, [rsi]            ; rcx = list2->head
-    mov [rdi], rcx
-    jmp .check_tail2
+    mov r8, rdx       ; guardamos el hash en r8
+    mov r9, [rdi]     ; r9 = list->first
+    mov r10, rsi      ; r10 = type
+    mov r11, rdx      ; r11 = hash original
 
-.else_case:
-    ; list1->tail->next = list2->head
-    mov rax, [rdi + 8]        ; rax = list1->tail
-    mov rcx, [rsi]            ; rcx = list2->head
-    mov [rax + 8], rcx        ; list1->tail->next = list2->head
+.loop:
+    test r9, r9       ; ¿current == NULL?
+    je .done          ; si sí, terminamos
 
-.check_tail2:
-    ; if (list2->tail != NULL)
-    mov rax, [rsi + 8]        ; rax = list2->tail
-    test rax, rax
-    je .end_concat
+    movzx rax, byte [r9 + 16] ; rax = current->type
+    cmp rax, r10      ; comparamos con el tipo
+    jne .next         ; si no son iguales, vamos al siguiente nodo
 
-    ; list1->tail = list2->tail
-    mov [rdi + 8], rax
+    ; Concatenamos el hash
+    mov rdi, r8       ; rdi = hash
+    mov rsi, [r9 + 24] ; rsi = current->hash
+    push r8
+    push r9
+    push r10
+    push r11
+    call str_concat
+    pop r11
+    pop r10
+    pop r9
+    pop r8
 
-.end_concat:
+    cmp r8, r11       ; comparamos el nuevo hash con el original
+    je .skip_free     ; si son iguales, no liberamos
+    mov rdi, r8       ; rdi = nuevo hash
+    push rax
+    push r9
+    push r10
+    push r11
+    call free         ; liberamos el hash anterior
+    pop r11
+    pop r10
+    pop r9
+    pop rax           ; recuperamos el nuevo hash
+
+.skip_free:
+    mov r8, rax       ; guardamos el nuevo hash en r8
+
+.next:
+    mov r9, [r9]      ; r9 = current->next
+    jmp .loop         ; volvemos al inicio del bucle
+
+.done:
+    ; Devolvemos el hash concatenado
+    mov rax, r8
     ret
